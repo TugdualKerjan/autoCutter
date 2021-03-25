1c008119ea360b3c93245e401f65c91ed39944e8
Responsable Services AGEPoly <responsable.services@agepoly.ch>
2021-03-25 11:23:01 +0100

    Lesssize?

--- main.py
+++ main.py
@@ -7,27 +7,26 @@ from telegram.ext import CommandHandler, MessageHandler, Updater, Filters, Callb
 from telegram.error import NetworkError, Unauthorized
 import numpy as np
 from io import BytesIO
+import os, psutil
+from guppy import hpy
 
+h = hpy()
+
+
+process = psutil.Process(os.getpid())
 updater = Updater(token='1607764973:AAHe1HbFf1JWYBUQUlPshRoOuUNng0fObvw', use_context=True)
 
 def resize(image):
     height, width, _ = image.shape
-    if width < 512:
-        if height < 512:
-            return image
-        else:
-            image = image[:512, :] #Truncate the height
-            return image
-
+    percent = (width / 512)
+    dim = (512, int(height / percent))
+    resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    height, width, _ = resized.shape
+    if height < 512:
+        return resized
     else:
-        percent = (width / 512)
-        dim = (512, int(height / percent))
-        resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        height, width, _ = resized.shape
-        if height < 512:
-            return resized
-        else:
-            return resized[:512, :] #Truncate the height
+        resized = resized[:512, :] #Truncate the height
+        return resized
 
 def main():
     dispatcher = updater.dispatcher
@@ -42,25 +41,30 @@ def receive_stickers(update, context):
     context.bot.deleteStickerFromSet(update.message.sticker['file_id'])
 
 def receive_images(update, context):
+    print (h.heap())
+
+    # print("MEM (MB): " + str(int(process.memory_info().rss/1000000)))
     user_id = int(update.message.from_user['id'])
     username = update.message.from_user['username']
 
 
-    file = context.bot.getFile(update.message.photo[-1].file_id)
-    f =  BytesIO(file.download_as_bytearray())
-    decode_img = cv2.imdecode(np.frombuffer(f.getbuffer(), np.uint8), -1)
+    decode_img = cv2.imdecode(np.frombuffer(BytesIO(context.bot.getFile(update.message.photo[-1].file_id).download_as_bytearray()).getbuffer(), np.uint8), -1)
+    # cv2.imdecode(np.fromstring(img_stream.read(), np.uint8), 1)
     
     for subimage in predict(decode_img):
+
+        # print("MEM (MB): " + str(int(process.memory_info().rss/1000000)))
         subimage = resize(subimage)
         buffer = cv2.imencode(".png", subimage)[1].tobytes()
         try: 
-            print("Adding to sticker pack")
-            print(context.bot.add_sticker_to_set(user_id, name="pepites_de_%s_by_faststicker_bot" % username, emojis="🧮", png_sticker=buffer))
+            # print("Adding to sticker pack with dim " + str(subimage.shape))
+            context.bot.add_sticker_to_set(user_id, name="pepites_de_%s_by_faststicker_bot" % username, emojis="🧮", png_sticker=buffer)
             context.bot.sendSticker(update.effective_chat.id, buffer)
         except:
-            print("Can't find sticker pack")
+            # print("Can't find sticker pack")
             context.bot.createNewStickerSet(user_id, name="pepites_de_%s_by_faststicker_bot" % username, title="PepitesDe%s" % username, png_sticker=buffer, emojis="🙂")
     context.bot.sendMessage(update.effective_chat.id, "Get your stickers at t.me/addstickers/pepites_de_%s_by_faststicker_bot" % username)
+    print (h.heap())
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
--- predictor.py
+++ predictor.py
@@ -13,11 +13,17 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
+# from vovnet import add_vovnet_config
+# model = "vovnet-detectron2/configs/mask_rcnn_V_19_FPNLite_3x.yaml"
+model = "COCO-InstanceSegmentation/mask_rcnn_R_101_DC5_3x.yaml"
+# model = "https://dl.dropbox.com/s/rptgw6stppbiw1u/vovnet19_ese_detectron2.pth"
 
-model = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
 
 cfg = get_cfg()
+# add_vovnet_config(cfg)
+# cfg.merge_from_file(model)
 cfg.merge_from_file(model_zoo.get_config_file(model))
+
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model)
 cfg.MODEL.DEVICE = "cpu"
@@ -38,15 +44,15 @@ def predict(image):
   masks = outputs["instances"].pred_masks.cpu().numpy()
   boxes = outputs["instances"].pred_boxes.tensor.cpu().numpy()
 
-  people = []
+  # people = []
 
   for x in range(0,len(outputs["instances"].pred_masks)):
     boxtemp = boxes[x].astype(int)
     masktemp = masks[x]
 
     if imsize(boxtemp) > image_size:
-      result = alphaimage * masktemp[..., None]
-      result = result[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2]]
-      people.append(result)
+      yield alphaimage[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2]] * masktemp[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2], None]
+      # result = result[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2]]
+      #  result
 
-  return people
+  # return people
