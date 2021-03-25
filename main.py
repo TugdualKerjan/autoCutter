c93547e259011051945ae3ae32929ba3fa1610b8
Responsable Services AGEPoly <responsable.services@agepoly.ch>
2021-03-25 12:22:31 +0100

    LIGHT

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from telegram.ext import CommandHandler, MessageHandler, Updater, Filters, Callb
 from telegram.error import NetworkError, Unauthorized
 import numpy as np
 from io import BytesIO
-# from guppy import hpy
 
-# h = hpy()
+from guppy import hpy
+h = hpy()
 
 updater = Updater(token='1607764973:AAHe1HbFf1JWYBUQUlPshRoOuUNng0fObvw', use_context=True)
 
@@ -38,30 +38,25 @@ def receive_stickers(update, context):
     context.bot.deleteStickerFromSet(update.message.sticker['file_id'])
 
 def receive_images(update, context):
-    # print (h.heap())
+    print (h.heap())
 
-    # print("MEM (MB): " + str(int(process.memory_info().rss/1000000)))
     user_id = int(update.message.from_user['id'])
     username = update.message.from_user['username']
 
-
     decode_img = cv2.imdecode(np.frombuffer(BytesIO(context.bot.getFile(update.message.photo[-1].file_id).download_as_bytearray()).getbuffer(), np.uint8), -1)
-    # cv2.imdecode(np.fromstring(img_stream.read(), np.uint8), 1)
+    context.bot.sendMessage(update.effective_chat.id, "Preparing scissors and glue...")
     
     for subimage in predict(decode_img):
 
-        # print("MEM (MB): " + str(int(process.memory_info().rss/1000000)))
         subimage = resize(subimage)
         buffer = cv2.imencode(".png", subimage)[1].tobytes()
         try: 
-            # print("Adding to sticker pack with dim " + str(subimage.shape))
             context.bot.add_sticker_to_set(user_id, name="pepites_de_%s_by_faststicker_bot" % username, emojis="🧮", png_sticker=buffer)
             context.bot.sendSticker(update.effective_chat.id, buffer)
         except:
-            # print("Can't find sticker pack")
             context.bot.createNewStickerSet(user_id, name="pepites_de_%s_by_faststicker_bot" % username, title="PepitesDe%s" % username, png_sticker=buffer, emojis="🙂")
     context.bot.sendMessage(update.effective_chat.id, "Get your stickers at t.me/addstickers/pepites_de_%s_by_faststicker_bot" % username)
-    # print (h.heap())
+    print (h.heap())
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
--- predictor.py
+++ predictor.py
@@ -13,20 +13,19 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
-from vovnet import add_vovnet_config
+# from vovnet import add_vovnet_config
 # model = "vovnet-detectron2/configs/mask_rcnn_V_19_FPNLite_3x.yaml"
-# model = "COCO-InstanceSegmentation/mask_rcnn_R_101_DC5_3x.yaml"
-modelconf = "https://raw.githubusercontent.com/youngwanLEE/vovnet-detectron2/master/configs/mask_rcnn_V_39_FPN_3x.yaml"
-model = "https://dl.dropbox.com/s/c5o3yr6lwrb1170/mask_V_39_eSE_ms_3x.pth"
+# model = "https://dl.dropbox.com/s/rptgw6stppbiw1u/vovnet19_ese_detectron2.pth"
+model = "COCO-InstanceSegmentation/mask_rcnn_R_50_C4_3x.yaml"
 
 
 cfg = get_cfg()
-add_vovnet_config(cfg)
+# add_vovnet_config(cfg)
 # cfg.merge_from_file(model)
-cfg.merge_from_file("configs/mask_rcnn_V_39_FPN_3x.yaml")
+cfg.merge_from_file(model_zoo.get_config_file(model))
 
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-cfg.MODEL.WEIGHTS = model
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model)
 cfg.MODEL.DEVICE = "cpu"
 
 predictor = DefaultPredictor(cfg)
@@ -45,15 +44,9 @@ def predict(image):
   masks = outputs["instances"].pred_masks.cpu().numpy()
   boxes = outputs["instances"].pred_boxes.tensor.cpu().numpy()
 
-  # people = []
-
   for x in range(0,len(outputs["instances"].pred_masks)):
     boxtemp = boxes[x].astype(int)
     masktemp = masks[x]
 
     if imsize(boxtemp) > image_size:
       yield alphaimage[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2]] * masktemp[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2], None]
-      # result = result[boxtemp[1]:boxtemp[3],boxtemp[0]:boxtemp[2]]
-      #  result
-
-  # return people
